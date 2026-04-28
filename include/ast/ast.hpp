d6a6bb502baee756254998ec5cdc8630242015a5
St4rgazer <St4rgazer0822@gmail.com>
2026-04-29 00:46:34 +0800

    refractor(ast) : delete old code

--- include/ast/ast.hpp
+++ include/ast/ast.hpp
@@ -7,27 +7,13 @@
 
 using namespace std;
 
-struct ASTNode {
-    string name;
-    string value;
-    int line = 0;
-    vector<ASTNode*> children;
-
-    ASTNode(string n, string v = "", int l = 0);
-};
-
-ASTNode* makeAstNode(const string& name, const string& value = "", int line = 0);
-ASTNode* makeAstNode(const string& name, initializer_list<ASTNode*> kids, const string& value = "", int line = 0);
-ASTNode* makeAstNode(const string& name, const vector<ASTNode*>& kids, const string& value = "", int line = 0);
-void printAst(ASTNode* node, int depth = 0);
-
 enum STYPE { SEMI, ASS, EXP, CONT, BRE, RET, BLK, SEL, ITER };
-enum UOP { UOP_ADD, UOP_MINUS, UOP_NOT };
-enum AOP { AOP_ADD, AOP_MINUS };
-enum MOP { MOP_MUL, MOP_DIV, MOP_MOD };
-enum ROP { ROP_GTE, ROP_LTE, ROP_GT, ROP_LT };
-enum EOP { EOP_EQ, EOP_NEQ };
-enum TYPE { TYPE_VOID, TYPE_INT, TYPE_FLOAT, TYPE_BOOL };
+enum UOP   { UOP_ADD, UOP_MINUS, UOP_NOT };
+enum AOP   { AOP_ADD, AOP_MINUS };
+enum MOP   { MOP_MUL, MOP_DIV, MOP_MOD };
+enum ROP   { ROP_GTE, ROP_LTE, ROP_GT, ROP_LT };
+enum EOP   { EOP_EQ, EOP_NEQ };
+enum TYPE  { TYPE_VOID, TYPE_INT, TYPE_FLOAT, TYPE_BOOL };
 
 class BaseAST;
 
--- src/ast/ast.cpp
+++ src/ast/ast.cpp
@@ -1,35 +1,5 @@
 #include "../../include/ast/ast.hpp"
 
-#include <iostream>
-#include <utility>
-
-ASTNode::ASTNode(std::string n, std::string v, int l) : name(std::move(n)), value(std::move(v)), line(l) {}
-
-ASTNode* makeAstNode(const std::string& name, const std::string& value, int line) {
-    return new ASTNode(name, value, line);
-}
-
-ASTNode* makeAstNode(const std::string& name, std::initializer_list<ASTNode*> kids, const std::string& value, int line) {
-    auto node = makeAstNode(name, value, line);
-    node->children.assign(kids.begin(), kids.end());
-    return node;
-}
-
-ASTNode* makeAstNode(const std::string& name, const std::vector<ASTNode*>& kids, const std::string& value, int line) {
-    auto node = makeAstNode(name, value, line);
-    node->children = kids;
-    return node;
-}
-
-void printAst(ASTNode* node, int depth) {
-    if (!node) return;
-    for (int i = 0; i < depth; ++i) std::cout << "  ";
-    std::cout << node->name;
-    if (!node->value.empty()) std::cout << ": " << node->value;
-    std::cout << '\n';
-    for (const auto& child : node->children) printAst(child, depth + 1);
-}
-
 void CompUnitAST::accept(Visitor &visitor) { visitor.visit(*this); }
 
 void DeclDefAST::accept(Visitor &visitor) { visitor.visit(*this); }
